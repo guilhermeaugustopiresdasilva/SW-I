aaefb789c13f23ca32d6ebd2c68b81b56f88c602
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-02-23 12:37:56 -0300

    Add files via upload

---
+++ exemplo/lampada/lampada/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            // INSTÂNCIA DE UM OBJETO
+            Lampada lampada1= new Lampada();
+
+            // ACESSANDO UM ATRIBUTO PUBLICO DP OBJETO 
+            lampada1.Ligada= true;
+            lampada1.Potencia = 150;
+            lampada1.Cor = "Vermelha";
+
+            // VIZUALIZANDO OS VALORES DE ATRIBUTOS
+            Console.WriteLine("A Lâmpada esta ligada? " + lampada1.Ligada);
+
+
+        }
+    }
+}
