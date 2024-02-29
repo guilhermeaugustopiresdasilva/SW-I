ae254a33174b9983a78c9b6378577db852da5e0a
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-02-29 12:27:57 -0300

    Add files via upload
    exercício-lâmpada na aula de SW

---
+++ SW-I-main/exemplo/lampada/lampada/Lampada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lampada
+{
+    public class Lampada
+    {
+        private bool ligada { get; set; }
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+        
+        public void ligar()
+        {
+            ligada = true;
+        }
+        public void desligar()
+        {
+            ligada = false;
+        }
+        public bool estaligada()
+        {
+            return ligada;
+        }
+    }
+}
