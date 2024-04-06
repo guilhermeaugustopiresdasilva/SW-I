b6968c5f81eb3954a5f80e24423948ad1e4e0292
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-04-06 20:23:39 -0300

    Add files via upload

---
+++ banco/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Conta
+    {
+        private float saldo_limite;
+
+        private string? Num_conta { get; set; }
+        private float Saldo { get; set; }
+        private float Limite { get; set; }
+
+        public float consulta_saldo()
+        { 
+            return Saldo;
+        }
+        public void deposito(float valor_deposito)
+        {
+            Saldo += valor_deposito;
+        }
+
+        public void sacar(float valor_saque)
+        {
+            if(valor_saque > saldo_limite)
+            {
+                Console.WriteLine("VOCE NÃO TEM SALDO SUFICIENTE");
+            }
+            else
+            {
+                Saldo -= valor_saque;
+                Console.WriteLine("Agora seu saldo é de R$" + consulta_saldo());
+            }
+        }
+
+        public void ajustar_limite(float valor_limite)
+        {
+            Limite = valor_limite;
+            Console.WriteLine("Agora seu limite é de R$" + Limite);
+            saldo_limite = Limite + Saldo;
+            Console.WriteLine("Seu saldo + Limite é de R$:" + saldo_limite);
+        }
+    }
+}
---
+++ banco/Program.cs
@@ -0,0 +1,19 @@
+﻿using Banco;
+
+namespace banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BANCO DA ISA!");
+
+            Conta conta = new Conta();
+            Console.WriteLine("Seu saldo inicial é de R$" + conta.consulta_saldo());
+
+            conta.deposito(500);
+            conta.ajustar_limite(500);
+            //Console.WriteLine("Seru saldo agora é de: R$" + conta.consulta_saldo());
+        }
+    }
+}
