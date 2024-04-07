000f58abf2dea7e4f15d18cdba761b4042abb37c
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-04-06 21:15:43 -0300

    Add files via upload

---
+++ Aviao/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aviao
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Controle Aviao = new Controle();
+
+            Aviao.Compania = "Decolar";
+            Aviao.Modelo = "Boing2411";
+            Aviao.Acelerar(40);
+            Aviao.Cima(19000);
+
+            Console.WriteLine($"Senhores passageiros, o avião está a { Aviao.Altura}pés, numa velocidade de {Aviao.Velocidade}kmh. Muito obgirado por nos escolher! Você está voando com {Aviao.Compania}, no modelo {Aviao.Modelo}. Uma boa viagem a todos");
+        }
+    }
+}
---
+++ Aviao/controle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aviao
+{
+    internal class Controle
+    {
+        public string? Compania { get; set; }
+        public string? Modelo { get; set; }
+        public int Velocidade { get; set; }
+        public int Altura { get; set; }
+
+
+        public void Cima(int pes)
+        {
+
+            Altura += pes;
+
+        }
+
+        public void Baixo(int pes)
+        {
+            if (pes >= 0)
+            {
+                Altura -= pes;
+            }
+        
+            else
+            {
+                Altura = 0;
+            }
+        }
+
+        public void Acelerar(int kmh)
+        {
+            Velocidade += kmh;
+
+        }
+
+        public void Desacelerar(int kmh)
+        {
+            if (kmh >= 0)
+            {
+                Velocidade -= kmh;
+            }
+
+            else
+            {
+                Velocidade = 0;
+            }
+
+        }
+
+    }
+}
