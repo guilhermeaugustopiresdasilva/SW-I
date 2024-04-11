5bd30104cdab9d61877893eb007531a52913c93e
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-04-11 11:29:01 -0300

    Add files via upload
    necessita finalização

---
+++ exercicio1/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+        public double AreaTotal { get; set;}
+
+        public string Calcular() 
+        {
+            Console.WriteLine("Digite a base: ");
+            Base = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a altura: ");
+            Altura = Convert.ToDouble(Console.ReadLine());
+            AreaTotal = Base * Altura;
+
+            string tipo = "";
+
+            if(Base == Altura) 
+            {
+                tipo = "Quadrado";            
+            }
+            else 
+            {
+                tipo = "Retângulo";
+            }
+
+            string resultado = "A área do seu " + tipo + "é:" + AreaTotal;
+            
+
+            return resultado;
+
+        }
+
+        public string Dados() 
+        {
+            string informacao = "O valor da base é: " + Base;
+            informacao += "\nO valor da altura é: " + Altura;
+
+            return informacao;
+        }
+
+    }
+}
