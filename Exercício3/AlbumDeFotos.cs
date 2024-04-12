d965541a2eaa0e8f30fa301431760724b24d8038
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-04-12 16:43:46 -0300

    Add files via upload

---
+++ Exercício3/AlbumDeFotos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício3
+{
+    class AlbumDeFotos
+    {
+        public int NumeroTotalDePaginas { get; set; } 
+        public int NumeroTotalDeFotos { get; set; }
+
+        private int numeroDeFotos;
+
+        public int NumeroDeFotos {
+            get
+            {
+                return numeroDeFotos;
+            }
+            set 
+            {
+                if (value > NumeroTotalDeFotos)
+                {
+                    Console.WriteLine("Erro, número de fotos muito alto");
+                }
+                else
+                {
+                    numeroDeFotos = value;
+                }
+            }
+        }
+
+        public void DefinirAlbum(int totalP, int fotos)
+        {
+            NumeroTotalDePaginas = totalP;
+            NumeroTotalDeFotos = totalP * 2;
+            NumeroDeFotos = fotos;
+            Console.WriteLine($"Número Total de Páginas: {NumeroTotalDePaginas}");
+            Console.WriteLine($"Número Total de Fotos: {NumeroTotalDeFotos}");
+            Console.WriteLine($"Número de Páginas: {NumeroDeFotos}");
+        }
+
+    }
+}
---
+++ Exercício3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercício3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           AlbumDeFotos af = new AlbumDeFotos();
+           af.DefinirAlbum(40, 71);
+           Console.WriteLine("------------------------------");
+           AlbumDeFotos af2 = new AlbumDeFotos();
+           af2.DefinirAlbum(40, 81);
+        }
+    }
+}
