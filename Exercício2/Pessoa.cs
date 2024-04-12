311cba38cdfb1f287f8bf6131b3a4906d7d6c0bb
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-04-12 16:42:33 -0300

    Add files via upload

---
+++ Exercício2/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício2
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public float Altura { get; set; }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Data de Nascimento: " + DataNascimento);
+            Console.WriteLine("Altura: " + Altura);
+        }
+
+
+        public int CalcularIdade()
+        {
+            return DateTime.Today.Year - DataNascimento.Year;
+        }
+    }
+}
---
+++ Exercício2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercício2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Joel";
+            p.DataNascimento = new DateTime(2008, 1, 19);
+            p.Altura = 1.76f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+            Console.ReadKey();
+        }
+    }
+}
