0b1eccd8c85b94cd65c924b9b3d6f9aac875d4d7
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-04-12 16:44:31 -0300

    Add files via upload

---
+++ Exercicío4/Pagamento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicío4
+{
+    class Pagamento
+    {
+        public string Nome { get; set; }
+        public float Sal { get; set; }
+        public float HED { get; set; }
+        public float HEN { get; set; }
+        public int ND { get; set; }
+        public float Fal { get; set; }
+        public float DE { get; set; }
+        public float REF { get; set; }
+        public float Val { get; set; }
+        
+        public string FolhaDePagamento()
+        {
+            double HoraExtra = HED * Sal / 160 + HEN * 1.2 * Sal / 160;
+            double SalF = ND * 0.05 * 1412;
+            double INAMPS = 0.08 * Sal;
+            double faltas = Fal * Sal / 160;
+            double SalB = Sal + HoraExtra + SalF;
+            double imposto_de_renda = 0.08 * Sal;
+
+            return $"Nome: {Nome}\nSalário: {Sal}\nHoras Extras: {HoraExtra}\nSalário Família: {SalF}\nSalário Bruto: {SalB}\nINAMPS: {INAMPS}\nFaltas: {faltas}\nRefeições: {REF}\nVales: {Val}\nDescontos Eventuais: {DE}\nImposto de renda: {imposto_de_renda}\nSalário Líquido: {SalB - INAMPS - faltas - REF - Val - DE - imposto_de_renda}";
+        }
+    }
+}
---
+++ Exercicío4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicío4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pagamento p = new Pagamento();
+            p.Nome = "Braia";
+            p.Sal = 4000;
+            p.HED = 0;
+            p.HEN = 24;
+            p.ND = 0;
+            p.Fal = 32;
+            p.DE = 5;
+            p.REF = 2;
+            p.Val = 0;
+            Console.WriteLine(p.FolhaDePagamento());
+        }
+    }
+}
