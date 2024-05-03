31ddcc743d98dcfc4036b80b60984ee5f1c4d5b8
Guilherme Augusto Pires da Silva <124791751+guilhermeaugustopiresdasilva@users.noreply.github.com>
2024-05-03 18:20:52 -0300

    Add files via upload

---
+++ ControleDeEstoque/ControleEstoque/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEstoque
+{
+    class Produto
+    {
+        private int codigo { get; set; }
+        private string nome { get; set; }
+
+        private int estoque;
+        private double valor { get; set; }
+
+        public void entrada(int qtd)
+        {
+            estoque += qtd;
+            Console.WriteLine("Entrada de " + qtd);
+        }
+
+        public void venda(int qtd)
+        {
+            estoque -= qtd;
+            Console.WriteLine("Venda de " + qtd);
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("Dados do produto");
+            Console.WriteLine("Cod: " + codigo);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Estoque: " + estoque);
+            Console.WriteLine("Valor: " + valor);
+        }
+
+        public Produto(int codigo, string nome, int estoque, double valor)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.estoque = estoque;
+            this.valor = valor;
+        }
+    }
+}
---
+++ ControleDeEstoque/ControleEstoque/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ControleEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto(1, "Notebook", 0, 2300);
+            produto.imprimir();
+            produto.entrada(20);
+            produto.imprimir();
+            produto.venda(5);
+            produto.imprimir();
+        }
+    }
+}
